2687ca84ff44c6d298b1a7f264c7f3701d0c3d3e
dojje <dojje@users.noreply.github.com>
2022-03-09 22:25:34 +0100

    added examples

---
+++ examples/reciever.rs
@@ -0,0 +1,34 @@
+use std::{fs::File, io, net::SocketAddr, str::FromStr};
+
+use dovepipe::{reciever::ProgressTracking, recv_file};
+use tokio::net::UdpSocket;
+
+#[tokio::main]
+async fn main() {
+    println!("Enter ip address and port of reciever:");
+    let mut reciever_ip_str = String::new();
+    io::stdin()
+        .read_line(&mut reciever_ip_str)
+        .expect("Failed to read input");
+
+    let reciever_ip_str = reciever_ip_str[0..reciever_ip_str.len()-2].to_string();
+
+    let reciever = SocketAddr::from_str(&reciever_ip_str).expect("not a valid ip address");
+
+    let my_ip = "0.0.0.0:7890";
+
+    let sock = UdpSocket::bind(my_ip)
+        .await
+        .expect("could not bind to address");
+
+    println!("my ip: {}", my_ip);
+
+    recv_file(
+        &mut File::create("output_from_recv.txt").expect("could not create output file"),
+        &sock,
+        reciever,
+        ProgressTracking::Memory,
+    )
+    .await
+    .expect("error when sending file");
+}
---
+++ examples/sender.rs
@@ -0,0 +1,35 @@
+use std::{io, net::SocketAddr, str::FromStr};
+
+use dovepipe::send_file;
+use tokio::net::UdpSocket;
+
+#[tokio::main]
+async fn main() {
+
+    // Read the ip of the reciever from stdin
+    println!("Enter ip address and port of sender: ");
+    let mut reciever_ip_str = String::new();
+    io::stdin()
+        .read_line(&mut reciever_ip_str)
+        .expect("Failed to read input");
+
+    let reciever_ip_str = reciever_ip_str[0..reciever_ip_str.len()-2].to_string();
+
+    // Create a udp socket
+
+    let reciever = SocketAddr::from_str(&reciever_ip_str).expect("not a valid ip address");
+
+    let my_ip = "0.0.0.0:3456";
+
+    let sock = UdpSocket::bind(my_ip)
+        .await
+        .expect("could not bind to address");
+
+    println!("my ip: {}", my_ip);
+
+    // Send the file with the send_file funciton
+    send_file(&sock, "./examples/file_to_send.txt", reciever)
+        .await
+        .expect("error when sending file");
+
+}
