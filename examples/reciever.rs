47caed009c927fcb46a862e50f8452aca778a869
dojje <dojje@users.noreply.github.com>
2022-03-12 23:15:27 +0100

    fixed printing typos in examples

--- examples/reciever.rs
+++ examples/reciever.rs
@@ -5,7 +5,9 @@ use tokio::net::UdpSocket;
 
 #[tokio::main]
 async fn main() {
-    println!("Enter ip address and port of reciever:");
+    let my_ip = "0.0.0.0:7890";
+    println!("my ip: {}", my_ip);
+    println!("Enter ip address and port of sender:");
     let mut reciever_ip_str = String::new();
     io::stdin()
         .read_line(&mut reciever_ip_str)
@@ -14,9 +16,6 @@ async fn main() {
     let reciever_ip_str = reciever_ip_str[0..reciever_ip_str.len()-2].to_string();
 
     let reciever = SocketAddr::from_str(&reciever_ip_str).expect("not a valid ip address");
-
-    let my_ip = "0.0.0.0:7890";
-    println!("my ip: {}", my_ip);
 
     let sock = UdpSocket::bind(my_ip)
         .await
--- examples/sender.rs
+++ examples/sender.rs
@@ -5,9 +5,11 @@ use tokio::net::UdpSocket;
 
 #[tokio::main]
 async fn main() {
+    let my_ip = "0.0.0.0:3456";
+    println!("my ip: {}", my_ip);
 
     // Read the ip of the reciever from stdin
-    println!("Enter ip address and port of sender: ");
+    println!("Enter ip address and port of reciever: ");
     let mut reciever_ip_str = String::new();
     io::stdin()
         .read_line(&mut reciever_ip_str)
@@ -19,8 +21,6 @@ async fn main() {
 
     let reciever = SocketAddr::from_str(&reciever_ip_str).expect("not a valid ip address");
 
-    let my_ip = "0.0.0.0:3456";
-    println!("my ip: {}", my_ip);
 
     let sock = UdpSocket::bind(my_ip)
         .await
